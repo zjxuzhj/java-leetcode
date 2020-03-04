126b2e5a32859abe42f968f70b811c5b938bf992
hongjay <hongjie.zhou@carplusgo.cn>
2020-03-04 16:34:58 +0800

    双指针法解决数组删除元素问题

---
+++ src/removeElement.java
@@ -0,0 +1,99 @@
+/**
+ * 27. 移除元素
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 nums = [3,2,2,3], val = 3,
+ * <p>
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ * <p>
+ * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+ * <p>
+ * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ * <p>
+ * 注意这五个元素可为任意顺序。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 说明:
+ * <p>
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * <p>
+ * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ * <p>
+ * 你可以想象内部操作如下:
+ * <p>
+ * // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+ * int len = removeElement(nums, val);
+ * <p>
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ */
+
+public class removeElement {
+
+    public static void main(String[] args) {
+        int nums[] = {3, 2, 2, 3};
+        System.out.println(removeElementNormal(nums, 2));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[i] != val) {
+                i++;
+            } else {
+                if (nums[i] != nums[j]) {
+                    nums[i] = nums[j];
+                    nums[j] = val;
+                    i++;
+                }
+            }
+        }
+        return i;
+    }
+
+    /**
+     * 算法---双指针法
+     * <p>
+     * 当 nums[j] 与给定的值相等时，递增 j 以跳过该元素。只要 nums[j] != val，我们就复制 nums[j] 到 nums[i] 并同时递增两个索引。
+     * 重复这一过程，直到 j 到达数组的末尾，该数组的新长度为 i。
+     * <p>
+     * 复杂度分析
+     * <p>
+     * 时间复杂度：O(n)，
+     * 假设数组总共有 n 个元素，i 和 j 至少遍历 2n 步。
+     * <p>
+     * 空间复杂度：O(1)。
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/remove-element/solution/yi-chu-yuan-su-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElementNormal(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
