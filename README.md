2944b03b917024dd5947eb234087e6f90001581d
hongjay <hongjie.zhou@carplusgo.cn>
2020-03-24 15:28:00 +0800

    二叉树层次遍历 使用层次遍历和深度遍历两种方法

--- README.md
+++ README.md
@@ -29,3 +29,4 @@
 11 | 100|<a href="https://github.com/zjxuzhj/java-leetcode/blob/master/src/sameTree.java"> 100. 相同的树</a> | 2020年3月12日 | 100.00% | 5.09%
 12 | 101|<a href="https://github.com/zjxuzhj/java-leetcode/blob/master/src/symmetricTree.java"> 101. 对称二叉树</a> | 2020年3月17日 | 100.00% | 13.13%
 13 | 104|<a href="https://github.com/zjxuzhj/java-leetcode/blob/master/src/maximumDepthOfBinaryTree.java"> 104. 二叉树的最大深度</a> | 2020年3月18日 | 100.00% | 28.68%
+14 | 107|<a href="https://github.com/zjxuzhj/java-leetcode/blob/master/src/binaryTreeLevelOrderTraversalIi.java"> 107. 二叉树的层次遍历 II</a> | 2020年3月24日 | 99.75% | 5.03%
---
+++ src/binaryTreeLevelOrderTraversalIi.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * <p>
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其自底向上的层次遍历为：
+ * <p>
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ */
+public class binaryTreeLevelOrderTraversalIi {
+    /**
+     * BFS层次遍历
+     * 执行用时 : 1 ms, 在所有 Java 提交中击败了 99.75% 的用户内存消耗 : 39.6 MB , 在所有 Java 提交中击败了 5.03% 的用户
+     */
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> integer1 = new ArrayList<>();
+        integer1.add(root.val);
+        lists.add(integer1);
+        while (!queue.isEmpty()) {
+            List<Integer> integerList = new ArrayList<>();
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode removeFirst = queue.removeFirst();
+                if (removeFirst.left != null) {
+                    queue.add(removeFirst.left);
+                    integerList.add(removeFirst.left.val);
+                }
+                if (removeFirst.right != null) {
+                    queue.add(removeFirst.right);
+                    integerList.add(removeFirst.right.val);
+                }
+            }
+            if (integerList.size() != 0) {
+                lists.add(integerList);
+            }
+        }
+        Collections.reverse(lists);
+        return lists;
+    }
+
+    /**
+     * DFS深度优先遍历
+     * 使用递归遍历二叉树框架的基本方法，同时添加 list 、 height 两个参数实现。
+     * 每次深度增加添加一个list，并且把当前层级的叶子节点添加到list中
+     */
+    public static List<List<Integer>> levelOrderBottomDFS(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+        levelOrderBottomDFS(root, lists, 0);
+        Collections.reverse(lists);
+        return lists;
+    }
+
+    public static List<List<Integer>> levelOrderBottomDFS(TreeNode root, List<List<Integer>> lists, int height) {
+        if (root == null) {
+            return null;
+        }
+        if (lists.size() <= height)
+            lists.add(new ArrayList<>());
+        lists.get(height).add(root.val);
+        height++;
+        levelOrderBottomDFS(root.left, lists, height);
+        levelOrderBottomDFS(root.right, lists, height);
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode p = new TreeNode(2);
+        TreeNode pLeft = new TreeNode(3);
+        TreeNode pRight = new TreeNode(4);
+        p.left = pLeft;
+        p.right = pRight;
+
+        TreeNode q = new TreeNode(2);
+        TreeNode qLeft = new TreeNode(4);
+        TreeNode qRight = new TreeNode(3);
+        q.left = qLeft;
+        q.right = qRight;
+
+        root.left = p;
+        root.right = q;
+        System.out.println(levelOrderBottomDFS(root));
+    }
+
+}
